be2988157a23cb929e9adb3e58faa05435505918
kirillmc <kirillmcdev@gmail.com>
2024-04-09 10:00:39 +0300

    added func for verify hash passwords

---
+++ pkg/verify_password/verify_password.go
@@ -0,0 +1,8 @@
+package verify_password
+
+import "golang.org/x/crypto/bcrypt"
+
+func VerifyPassword(hashedPassword string, candidatePassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(candidatePassword))
+	return err == nil
+}
